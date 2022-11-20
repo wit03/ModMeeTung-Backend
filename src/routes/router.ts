cfe4b935422f16561c1fb43aed1b5a521688aad4
wit03 <witjarukit@gmail.com>
2022-11-20 16:01:33 +0700

    feat: search and update paths

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,6 +4,7 @@ import { User } from '../models/user'
 
 const router = express.Router();
 
+//GET all users
 router.get('/api/users', (req, res) => {
     User.find({}, (err, data) => {
         if (!err) {
@@ -14,6 +15,7 @@ router.get('/api/users', (req, res) => {
     });
 });
 
+//Create new user
 router.post('/api/users/add', (req, res) =>{
     const usr = new User({
         lineId: req.body.lineId,
@@ -27,4 +29,27 @@ router.post('/api/users/add', (req, res) =>{
     })
 })
 
+//Find specific user
+router.get('/api/users/:id', (req, res) => {
+    const { id }: any = req.params
+    User.find({ lineId: id }, (err, data) => {
+        if (!err) {
+            res.send(data);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+//Update specific user
+router.put('/api/users/update/:id', async (req, res) => {
+    const { id }: any = req.params
+    try {
+        await User.findByIdAndUpdate({ lineId: id }, req.body)
+        res.send("Update success");
+    } catch (e) {
+        res.status(500).send(e)
+    }
+    
+})
 export default router
